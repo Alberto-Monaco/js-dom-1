d57fb0e0a5a7954fe6c01e52c35cb4e6fa11b475
Alberto-Monaco <alberto.monaco84@gmail.com>
2024-10-08 14:13:39 +0200

    complete exercise

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
 /*Consegna
 Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato e accanto un bottone con la scritta “Accendi”.
 Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato)*/
+
+const lamp_whiteEl = document.getElementById('lamp_white')
+const btnEl = document.getElementById('btn')
+console.log(lamp_whiteEl)
+console.log(btnEl)
+
+btnEl.addEventListener('click', function () {
+	lamp_whiteEl.src = './assets/img/yellow_lamp.png'
+})
