6ccdedd923c3e045fb7a5791b3ed173f8d16610f
Alberto-Monaco <alberto.monaco84@gmail.com>
2024-10-08 16:26:18 +0200

    fix comment

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 //*Consegna
-//*Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato e accanto un bottone con la scritta “Accendi”.
-//*Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato)
+/*Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato e accanto un bottone con la scritta “Accendi”.
+Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato)*/
 //?BONUS
-//?Facciamo accendi e spegni:
-//?Al primo click la lampadina si accende e nel bottone compare la scritta "Spegni"
-//?Al secondo click la lampadina si spegne e nel bottone compare la scritta "Accendi"
-//?E così via...
-//?Per fare questo bonus potremmo aver bisogno del metodo string.includes()
+/*Facciamo accendi e spegni:
+Al primo click la lampadina si accende e nel bottone compare la scritta "Spegni"
+Al secondo click la lampadina si spegne e nel bottone compare la scritta "Accendi"
+E così via...
+Per fare questo bonus potremmo aver bisogno del metodo string.includes()*/
 
 const lampEl = document.getElementById('lamp')
 const btnEl = document.getElementById('btn')
